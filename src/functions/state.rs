d85d998bbbc78ea9c6bdca515aceb856ee58464b
Marcelo <nextlevlcode@gmail.com>
2024-12-30 13:53:24 -0300

    path

--- src/functions/state.rs
+++ src/functions/state.rs
@@ -1,5 +1,5 @@
 use std::{
-    fs::{create_dir_all, File, OpenOptions},
+    fs::{create_dir_all, OpenOptions},
     io::{BufReader, Write},
 };
 
@@ -16,9 +16,13 @@ use super::relative_path::relative_path;
 const STATE_FILE: &'static str = "/etc/lib/myarchinstall/state.json";
 
 pub fn load_state() -> Result<State> {
-    let mut file_state = File::create(&STATE_FILE)?;
-    file_state.write_all(b"{\"step\": 0}")?;
-    if let Ok(file) = OpenOptions::new().read(true).open(&STATE_FILE) {
+    if let Ok(mut file) = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .read(true)
+        .open(&STATE_FILE)
+    {
+        file.write_all(b"{\"step\": 0}")?;
         let reader = BufReader::new(&file);
 
         match from_reader(reader) {
